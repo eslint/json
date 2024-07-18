f7d830fff31a1a260d54da3033afc02dbbfd87b3
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-07-18 10:07:35 -0400

    chore: Add meta info to plugin

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ import noEmptyKeys from "./rules/no-empty-keys.js";
 //-----------------------------------------------------------------------------
 
 const plugin = {
+	meta: {
+		name: "@eslint/json",
+		version: "0.1.0", // x-release-please-version
+	},
 	languages: {
 		json: new JSONLanguage({ mode: "json" }),
 		jsonc: new JSONLanguage({ mode: "jsonc" }),
