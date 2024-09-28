19c1f2dfff18e4901d13cd452506cf64ecf0e862
Milos Djermanovic <milos.djermanovic@gmail.com>
2024-09-28 20:55:12 +0200

    feat: Add support for config comments

--- src/languages/json-source-code.js
+++ src/languages/json-source-code.js
@@ -8,7 +8,12 @@
 //-----------------------------------------------------------------------------
 
 import { iterator } from "@humanwhocodes/momoa";
-import { VisitNodeStep, TextSourceCodeBase } from "@eslint/plugin-kit";
+import {
+	VisitNodeStep,
+	TextSourceCodeBase,
+	ConfigCommentParser,
+	Directive,
+} from "@eslint/plugin-kit";
 
 //-----------------------------------------------------------------------------
 // Types
@@ -23,10 +28,18 @@ import { VisitNodeStep, TextSourceCodeBase } from "@eslint/plugin-kit";
 /** @typedef {import("@eslint/core").TraversalStep} TraversalStep */
 /** @typedef {import("@eslint/core").TextSourceCode} TextSourceCode */
 /** @typedef {import("@eslint/core").VisitTraversalStep} VisitTraversalStep */
+/** @typedef {import("@eslint/core").FileProblem} FileProblem */
+/** @typedef {import("@eslint/core").DirectiveType} DirectiveType */
+/** @typedef {import("@eslint/core").RulesConfig} RulesConfig */
 
 //-----------------------------------------------------------------------------
 // Helpers
 //-----------------------------------------------------------------------------
+
+const commentParser = new ConfigCommentParser();
+
+const INLINE_CONFIG =
+	/^\s*(?:eslint(?:-enable|-disable(?:(?:-next)?-line)?)?)(?:\s|$)/u;
 
 /**
  * A class to represent a step in the traversal process.
@@ -73,6 +86,12 @@ export class JSONSourceCode extends TextSourceCodeBase {
 	#parents = new WeakMap();
 
 	/**
+	 * Collection of inline configuration comments.
+	 * @type {Array<JSONToken>}
+	 */
+	#inlineConfigComments;
+
+	/**
 	 * The AST of the source code.
 	 * @type {DocumentNode}
 	 */
@@ -96,6 +115,135 @@ export class JSONSourceCode extends TextSourceCodeBase {
 		this.comments = ast.tokens
 			? ast.tokens.filter(token => token.type.endsWith("Comment"))
 			: [];
+	}
+
+	/**
+	 * Returns the value of the given comment.
+	 * @param {JSONToken} comment The comment to get the value of.
+	 * @returns {string} The value of the comment.
+	 * @throws {Error} When an unexpected comment type is passed.
+	 */
+	#getCommentValue(comment) {
+		if (comment.type === "LineComment") {
+			return this.getText(comment).slice(2); // strip leading `//`
+		}
+
+		if (comment.type === "BlockComment") {
+			return this.getText(comment).slice(2, -2); // strip leading `/*` and trailing `*/`
+		}
+
+		throw new Error(`Unexpected comment type '${comment.type}'`);
+	}
+
+	/**
+	 * Returns an array of all inline configuration nodes found in the
+	 * source code.
+	 * @returns {Array<JSONToken>} An array of all inline configuration nodes.
+	 */
+	getInlineConfigNodes() {
+		if (!this.#inlineConfigComments) {
+			this.#inlineConfigComments = this.comments.filter(comment =>
+				INLINE_CONFIG.test(this.#getCommentValue(comment)),
+			);
+		}
+
+		return this.#inlineConfigComments;
+	}
+
+	/**
+	 * Returns directives that enable or disable rules along with any problems
+	 * encountered while parsing the directives.
+	 * @returns {{problems:Array<FileProblem>,directives:Array<Directive>}} Information
+	 *      that ESLint needs to further process the directives.
+	 */
+	getDisableDirectives() {
+		const problems = [];
+		const directives = [];
+
+		this.getInlineConfigNodes().forEach(comment => {
+			const { label, value, justification } =
+				commentParser.parseDirective(this.#getCommentValue(comment));
+
+			// `eslint-disable-line` directives are not allowed to span multiple lines as it would be confusing to which lines they apply
+			if (
+				label === "eslint-disable-line" &&
+				comment.loc.start.line !== comment.loc.end.line
+			) {
+				const message = `${label} comment should not span multiple lines.`;
+
+				problems.push({
+					ruleId: null,
+					message,
+					loc: comment.loc,
+				});
+				return;
+			}
+
+			switch (label) {
+				case "eslint-disable":
+				case "eslint-enable":
+				case "eslint-disable-next-line":
+				case "eslint-disable-line": {
+					const directiveType = label.slice("eslint-".length);
+
+					directives.push(
+						new Directive({
+							type: /** @type {DirectiveType} */ (directiveType),
+							node: comment,
+							value,
+							justification,
+						}),
+					);
+				}
+
+				// no default
+			}
+		});
+
+		return { problems, directives };
+	}
+
+	/**
+	 * Returns inline rule configurations along with any problems
+	 * encountered while parsing the configurations.
+	 * @returns {{problems:Array<FileProblem>,configs:Array<{config:{rules:RulesConfig},loc:SourceLocation}>}} Information
+	 *      that ESLint needs to further process the rule configurations.
+	 */
+	applyInlineConfig() {
+		const problems = [];
+		const configs = [];
+
+		this.getInlineConfigNodes().forEach(comment => {
+			const { label, value } = commentParser.parseDirective(
+				this.#getCommentValue(comment),
+			);
+
+			if (label === "eslint") {
+				const parseResult = commentParser.parseJSONLikeConfig(value);
+
+				if (parseResult.ok) {
+					configs.push({
+						config: {
+							rules: parseResult.config,
+						},
+						loc: comment.loc,
+					});
+
+					// Explicit check needed for TypeScript
+				} else if (parseResult.ok === false) {
+					problems.push({
+						ruleId: null,
+						message: parseResult.error.message,
+						loc: comment.loc,
+					});
+				}
+			}
+		});
+
+		return {
+			configs,
+			problems,
+		};
 	}
 
 	/**
