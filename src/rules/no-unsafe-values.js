1592e51a0c4cfb1cfa454a486f0ad6fc62ed0e97
Joe Hildebrand <joe-github@cursive.net>
2024-12-03 14:36:41 -0700

    Rework unsafe zero.  Fix code review issues.

--- src/rules/no-unsafe-values.js
+++ src/rules/no-unsafe-values.js
@@ -3,9 +3,11 @@
  * @author Bradley Meck Farias
  */
 
-// These numbers evaluated to zero unexpectedly.  If the group's digits
-// are all zero, that's fine.
-const SMALL_NUMBERS = [/\.(\d+)/u, /((?:\d+\.)?\d+)e/iu];
+// RFC 8259's `number` production, as a regex.  Capture the integer part
+// and the fractional part.
+const NUMBER =
+	/^-?(?<int>0|([1-9][0-9]*))(?:\.(?<frac>[0-9]+))?(?:[eE][+-]?[0-9]+)?$/u;
+const NON_ZERO = /[1-9]/u;
 
 export default {
 	meta: {
@@ -18,7 +20,10 @@ export default {
 		messages: {
 			unsafeNumber: "Number outside safe range found.",
 			unsafeInteger: "Integer outside safe range found.",
-			subnormal: "Subnormal numbers are outside the safe range.",
+			unsafeZero:
+				"This number will evaluate to zero, which is unintended.",
+			subnormal:
+				"Subnormal numbers (see https://en.wikipedia.org/wiki/Subnormal_number) are outside the safe range: '{{ value }}'.",
 			loneSurrogate: "Lone surrogate '{{ surrogate }}' found.",
 		},
 	},
@@ -33,20 +38,26 @@ export default {
 					});
 				} else {
 					const txt = context.sourceCode.getText(node);
+
+					// Also matches -0, intentionally
 					if (node.value === 0) {
-						// If the value has been rounded down to 0, but there was some fraction
-						// or non-zero part before an e-, this is a very small number that doesn't
-						// fit inside an f64.
-						for (const r of SMALL_NUMBERS) {
-							const m = txt.match(r);
-							// If the digits are all 0, it's ok.
-							if (m?.[1]?.match(/[1-9]/u)) {
-								context.report({
-									loc: node.loc,
-									messageId: "unsafeNumber",
-								});
-								break;
-							}
+						// If the value has been rounded down to 0, but there was some
+						// fraction or non-zero part before the e-, this is a very small
+						// number that doesn't fit inside an f64.
+						const match = txt.match(NUMBER);
+						// assert(match, "If the regex is right, match is always truthy")
+
+						// If any part of the number other than the  has a non-zero digit
+						// in it, this number was not intended to be evaluated down to a
+						// zero.
+						if (
+							NON_ZERO.test(match.groups.int) ||
+							NON_ZERO.test(match.groups.frac)
+						) {
+							context.report({
+								loc: node.loc,
+								messageId: "unsafeZero",
+							});
 						}
 					} else if (!txt.match(/[.e]/iu)) {
 						// Intended to be an integer
@@ -61,15 +72,17 @@ export default {
 						}
 					} else {
 						// Floating point.  Check for subnormal.
-						const ab = new ArrayBuffer(8);
-						const dv = new DataView(ab);
-						dv.setFloat64(0, node.value, false);
-						const bi = dv.getBigUint64(0, false);
+						const buffer = new ArrayBuffer(8);
+						const view = new DataView(buffer);
+						view.setFloat64(0, node.value, false);
+						const asBigInt = view.getBigUint64(0, false);
 						// Subnormals have an 11-bit exponent of 0 and a non-zero mantissa.
-						if ((bi & 0x7ff0000000000000n) === 0n) {
+						if ((asBigInt & 0x7ff0000000000000n) === 0n) {
 							context.report({
 								loc: node.loc,
 								messageId: "subnormal",
+								// Value included so that it's seen in scientific notation
+								data: node,
 							});
 						}
 					}
