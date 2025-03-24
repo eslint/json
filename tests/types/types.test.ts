d099c78318f8ca9a426d233717728304418425a1
Brody McKee <mrmckeb@users.noreply.github.com>
2025-03-25 01:21:50 +1100

    fix: Update `types.ts` for compatibility with `verbatimModuleSyntax` (#88)
    * feat: Update `types.ts` for compatibility with `verbatimModuleSyntax`
    
    * Add test for imported types
    
    * Update tsconfig

--- tests/types/types.test.ts
+++ tests/types/types.test.ts
@@ -1,5 +1,6 @@
 import json from "@eslint/json";
 import { ESLint } from "eslint";
+import type { JSONSyntaxElement } from "@eslint/json/types";
 
 json satisfies ESLint.Plugin;
 json.meta.name satisfies string;
@@ -21,3 +22,14 @@ json.configs.recommended.plugins satisfies object;
 	// Check that all recommended rule names match the names of existing rules in this plugin.
 	null as AssertAllNamesIn<RecommendedRuleName, RuleName>;
 }
+
+// Check that types are imported correctly from `@humanwhocodes/momoa`.
+({
+	start: { line: 1, column: 1, offset: 1 },
+	end: { line: 1, column: 1, offset: 1 },
+}) satisfies JSONSyntaxElement["loc"];
+({
+	// @ts-expect-error -- This is not a valid Location.
+	start: 100,
+	end: { line: 1, column: 1, offset: 1 },
+}) satisfies JSONSyntaxElement["loc"];
