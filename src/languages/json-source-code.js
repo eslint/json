b0d55b81cd51a5d29ce93fc5e04851100ed5e11b
루밀LuMir <rpfos@naver.com>
2025-07-18 17:21:19 +0900

    refactor: add missing type annotations to variables

--- src/languages/json-source-code.js
+++ src/languages/json-source-code.js
@@ -197,7 +197,9 @@ export class JSONSourceCode extends TextSourceCodeBase {
 	 *      that ESLint needs to further process the directives.
 	 */
 	getDisableDirectives() {
+		/** @type {Array<FileProblem>} */
 		const problems = [];
+		/** @type {Array<Directive>} */
 		const directives = [];
 
 		this.getInlineConfigNodes().forEach(comment => {
@@ -250,7 +252,9 @@ export class JSONSourceCode extends TextSourceCodeBase {
 	 *      that ESLint needs to further process the rule configurations.
 	 */
 	applyInlineConfig() {
+		/** @type {Array<FileProblem>} */
 		const problems = [];
+		/** @type {Array<{config:{rules:RulesConfig},loc:SourceLocation}>} */
 		const configs = [];
 
 		this.getInlineConfigNodes().forEach(comment => {
