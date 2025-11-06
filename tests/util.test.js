b9e21bcb079bb02f9064eaa282a02a9256568337
루밀LuMir <rpfos@naver.com>
2025-11-06 20:50:20 +0900

    wip

--- tests/util.test.js
+++ tests/util.test.js
@@ -8,12 +8,9 @@
 //------------------------------------------------------------------------------
 
 import assert from "node:assert";
-// import { JSONLanguage } from "../../src/languages/json-language.js";
-// import { JSONSourceCode } from "../../src/languages/json-source-code.js";
-import {
-	getKey,
-	// getRawKey
-} from "../src/util.js";
+import { JSONLanguage } from "../src/languages/json-language.js";
+import { JSONSourceCode } from "../src/languages/json-source-code.js";
+import { getKey, getRawKey } from "../src/util.js";
 
 //------------------------------------------------------------------------------
 // Tests
@@ -38,19 +35,31 @@ describe("util", () => {
 		});
 	});
 
-	/*
 	describe("getRawKey()", () => {
 		const file = { body: `{"foo": 1, 'bar': 2, baz: 3}` };
-		const language = new JSONLanguage({ mode: "jsonc" });
+		const language = new JSONLanguage({ mode: "json5" });
 		const parseResult = language.parse(file);
 		const sourceCode = new JSONSourceCode({
 			text: file.body,
 			ast: parseResult.ast,
 		});
 
-		it("TODO", () => {
-			// TODO
+		it("should return correct raw key for `String` nodes with double quotes", () => {
+			const fooNode = parseResult.ast.body.members[0];
+
+			assert.strictEqual(getRawKey(fooNode, sourceCode), "foo");
+		});
+
+		it("should return correct raw key for `String` nodes with single quotes", () => {
+			const barNode = parseResult.ast.body.members[1];
+
+			assert.strictEqual(getRawKey(barNode, sourceCode), "bar");
+		});
+
+		it("should return correct raw key for `Identifier` nodes", () => {
+			const bazNode = parseResult.ast.body.members[2];
+
+			assert.strictEqual(getRawKey(bazNode, sourceCode), "baz");
 		});
 	});
-    */
 });
